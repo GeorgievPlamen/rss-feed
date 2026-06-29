7b952b47cdfcdf8302a287149deae639827b66f0
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2026-06-29 13:18:45 +0300

    setup psql

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE
+    IF NOT EXISTS users (
+        id UUID,
+        created_at TIMESTAMP,
+        updated_at TIMESTAMP,
+        name text Unique NOT NULL
+    );
+
+-- +goose Down
+DROP TABLE users;
